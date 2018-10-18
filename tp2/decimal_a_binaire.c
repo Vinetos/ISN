4a21e7e19493e9676b704510be03771180de5e55
Valentin Chassignol <Vinetos@users.noreply.github.com>
2018-10-18 11:27:14 +0200

    Update decimal_a_binaire.c

--- tp2/decimal_a_binaire.c
+++ tp2/decimal_a_binaire.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-// Convertit un nombre binaire en decimal
-int convertBinaryToDecimal(long long n);
+// Convertit un nombre decimal en binaire
+long long convertDecimalToBinary(int n);
 
 int main() {
-    long long number;
-    printf("Donnez un nombre binnaire: ");
-    scanf_s("%lld", &number);
-    printf("Le nombre est %d", convertBinaryToDecimal(number));
+    int number;
+    printf("Donnez un nombre decimale : ");
+    scanf_s("%d", &number);
+    printf("Le nombre en binaire est %lld", convertDecimalToBinary(number));
     return 0;
 }
 /**
- * Convertit un nombre binaire en decimal
- * @param number le nombre binaire
- * @return la valeur en decimal
+ * Convertit un nombre decimal en binaire
+ * @param number le nombre decimal
+ * @return la valeur en binaire
  */
-int convertBinaryToDecimal(long long number) {
-    int decimalNumber = 0, power = 0;
+long long convertDecimalToBinary(int number) {
+    long long binary = 0;
+    int column_of_digit = 1;
     while (number != 0) {
-        int remainder = number % 10;
-        number /= 10;
-        decimalNumber += remainder * pow(2, power);
-        ++power;
+        int remainder = number % 2;
+        number /= 2;
+        binary += remainder * column_of_digit;
+        column_of_digit *= 10;
     }
-    return decimalNumber;
+    return binary;
 }
